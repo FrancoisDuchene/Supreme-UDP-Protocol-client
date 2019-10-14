9510d836a11a4011c782b1e2853aa14b7ab7a09c
reginsterg <guillaumereg@hotmail.fr>
2019-10-14 12:04:48 +0200

    amélioration de fonctions chat

--- chat/create_socket.c
+++ chat/create_socket.c
@@ -13,55 +13,55 @@
  * @return: a file descriptor number representing the socket,
  *         or -1 in case of error (explanation will be printed on stderr)
  */
-int create_socket(struct sockaddr_in6 *source_addr,
-                 int src_port,
-                 struct sockaddr_in6 *dest_addr,
-                 int dst_port) {
-		int sockfd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
-		if(sockfd < 0) {
-			perror("Socket create failed");
+int create_socket(struct sockaddr_in6 *source_addr,int src_port,struct sockaddr_in6 *dest_addr,int dst_port) {
+
+	int sockfd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+	if(sockfd < 0) {
+        fprintf(stderr, "Socket create failed\n");
+		return -1;
+	}
+
+	int client = 0; 
+	if(source_addr == NULL) {
+		if(dest_addr == NULL) {
+			fprintf(stderr, "source_addr and dest_addr are NULL\n");
+			return -1;
+		} else {
+			if(dst_port <= 0) {
+				fprintf(stderr, "Destination port not correctly set\n");
+				return -1;
+			}
+			client = 1;
+		}
+	} else {
+		if(dest_addr == NULL) {
+			if(src_port <= 0) {
+				fprintf(stderr, "Source port not correctly set\n");
+				return -1;
+			}
+			client = 0;
+		} else {
+			fprintf(stderr, "source_addr and dest_addr are both non NULL\n");
 			return -1;
 		}
-		int client = 1; // true
-		if(source_addr == NULL) {
-				if(dest_addr == NULL) {
-					fprintf(stderr, "source_addr and dest_addr are NULL\n");
-					return -1;
-				} else {
-					if(dst_port <= 0) {
-						fprintf(stderr, "Destination port not correctly set\n");
-						return -1;
-					}
-					client = 1;
-				}
-		} else {
-				if(dest_addr == NULL) {
-					if(src_port <= 0) {
-						fprintf(stderr, "Source port not correctly set\n");
-						return -1;
-					}
-					client = 0;
-				} else {
-					fprintf(stderr, "source_addr and dest_addr are both non NULL\n");
-					return -1;
-				}
+	}
+	
+	if (client) {
+		dest_addr->sin6_port = htons(dst_port);
+        dest_addr->sin6_addr = in6addr_loopback;
+		if(connect(sockfd, (struct sockaddr *) dest_addr, sizeof(struct sockaddr_in6)) == -1) {
+            fprintf(stderr, "Connecting socked failed\n");
+			close(sockfd);
+			return -1;
 		}
-		if (client) {
-			dest_addr->sin6_port = htons(dst_port);
-			if(connect(sockfd, (struct sockaddr *) dest_addr, sizeof(struct sockaddr_in6)) == -1) {
-				perror("Connecting socked failed");
-				close(sockfd);
-				return -1;
-			}
-		} else {
-			source_addr->sin6_port = htons(src_port);
-      source_addr->sin6_addr = in6addr_any;
-			if(bind(sockfd, (struct sockaddr *) source_addr, sizeof(struct sockaddr_in6)) == -1) {
-				perror("Binding didn't worked");
-				close(sockfd);
-				return -1;
-			}
+	} else {
+		source_addr->sin6_port = htons(src_port);
+      	source_addr->sin6_addr = in6addr_any;
+		if(bind(sockfd, (struct sockaddr *) source_addr, sizeof(struct sockaddr_in6)) == -1) {
+         	fprintf(stderr, "Binding didn't worked\n");
+			close(sockfd);
+			return -1;
 		}
-
-		return sockfd;
+	}
+	return sockfd;
 }
--- chat/read_write_loop.c
+++ chat/read_write_loop.c
@@ -3,6 +3,7 @@
 #include <sys/select.h>
 #include <netdb.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "read_write_loop.h"
 
@@ -12,51 +13,48 @@
  * @return: as soon as stdin signals EOF
  */
 void read_write_loop(int sfd){
+
 	fd_set readsfd;
-	fd_set writesfd;
 	struct timeval timeout;
 	char buf[1024];
-	char buf2[1024];
+    char buf2[1024];
 
 	timeout.tv_sec = 3;
 	timeout.tv_usec = 500000;
-
-	FD_ZERO(&readsfd);
- 	FD_ZERO(&writesfd);
+    
+    FD_ZERO(&readsfd);
+    FD_SET(STDIN_FILENO, &readsfd);
 	FD_SET(sfd, &readsfd);
-	FD_SET(sfd, &writesfd);
 
-	size_t endOfStream = 1;
-
-	while (endOfStream) { // When EOF is reached, fread returns 0
-
-		if (select(sfd + 1, &readsfd, &writesfd, NULL, &timeout) == -1) {
+	while (1) { 
+       
+		if (select(sfd + 1, &readsfd, NULL, NULL, &timeout) == -1) {
 			printf (" Error select \n");
 			return ;
 		}
-
-		endOfStream = fread(buf, strlen(buf)+1, 1, stdin);
-		if (buf != NULL) {
-			if(FD_ISSET(sfd, &writesfd)){
-				if(send(sfd,(void *) buf,sizeof(buf),0)==-1){
-					printf(" Error recv \n");
-					return;
-				}
+        
+        if(FD_ISSET(STDIN_FILENO, &readsfd)){
+            ssize_t readL = read(STDIN_FILENO,buf,sizeof(buf));
+            
+			if(readL == EOF){
+				break;
 			}
-		}
+            
+            size_t readLen = (size_t)readL;
 
-		if(FD_ISSET(sfd, &readsfd)){
-			if(recv(sfd,(void*)buf2,sizeof(buf2),0)==-1){
-				printf(" Error recv \n");
+			if((int) write(sfd,buf,readLen) == -1){
+                printf(" Error write1 \n");
 				return;
-			}
-
-			if(strcmp(buf , "Q") == 0){
-				printf("End\n");
-                break;
-			}
-			fwrite(buf2, strlen(buf2)+1, 1, stdout);
+            }
 		}
+		
+        if(FD_ISSET(sfd, &readsfd)){
+            size_t readLen = (size_t)read(sfd,buf2,sizeof(buf2));
 
-	}
+			if((int) write(STDOUT_FILENO,buf2,readLen) == -1){
+                printf(" Error write2 \n");
+				return;
+            }
+		}    
+    }
 }
